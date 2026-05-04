c110f5f8424b1ada649ca3fe26d38e27c896ffa1
Meng-Chieh Chang <marcuschang0505@gmail.com>
2026-05-04 15:57:02 +1000

    Remove Microsoft.AspNetCore.OpenApi and calls

--- src/Api/JobProcessing.Api/Program.cs
+++ src/Api/JobProcessing.Api/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,7 +12,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
